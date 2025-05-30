911bd6618e69bfa0aaa20c0a0c0361dd746fc629
jorabek710ly <jorabekabdurashidov555@gmail.com>
2025-05-30 15:27:32 +0500

    louder

--- src/utils/utils.jsx
+++ src/utils/utils.jsx
@@ -1,17 +1,15 @@
+// components/SuspenseCustom.jsx
 import { Suspense } from "react";
-import "./utils.css";
+import "../styles/loader.css"; // Stilini import qilamiz
+
 const LoadingSuspense = () => {
-    return (
-        <div className="w-full h-screen z-50 bg-primary-bg flex items-center justify-center">
-            <div className="loader"></div>
-        </div>
-    )
-}
+  return (
+    <div className="w-full h-screen flex items-center justify-center bg-white z-50">
+      <div className="loader"></div>
+    </div>
+  );
+};
 
 export const SuspenseCustom = ({ children }) => {
-    return (
-        <Suspense fallback={<LoadingSuspense />}>
-            {children}
-        </Suspense>
-    )
-}
+  return <Suspense fallback={<LoadingSuspense />}>{children}</Suspense>;
+};
